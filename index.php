9f69506fd34903f071e32a76e260d0174b4b7a37
Chad Davis <dynamiccookies@users.noreply.github.com>
2024-08-06 21:55:48 -0500

    Increment version to v1.3.0

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Define constant for the current version
  */
 
-define('CURRENT_VERSION', 'v1.2.3');
+define('CURRENT_VERSION', 'v1.3.0');
 
 /******************************************************************************/
 
